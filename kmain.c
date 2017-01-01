4ac0140a7994ba334cfba45683b38ef1b4b601a6
Josh Simmons <josh.simmons@gmail.com>
2016-12-31 18:18:18 -0800

    Color test.

--- kmain.c
+++ kmain.c
@@ -3,34 +3,13 @@
 int kmain() {
   fb_set_color(15, 0);
   fb_clear();
-  char str[] =
-    "1 hello, how are you?\n"
-    "2 this is quite an auspicious day.\n"
-    "3 new years eve, in fact!\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "\n"
-    "24\n"
-    "25\n"
-    "26 combo breaker!";
-  fb_write(str, sizeof(str));
+  for (int y = 0; y < 16; ++y) {
+    for (int x = 0; x < 16; ++x) {
+      fb_set_color(x, y);
+      fb_write("Lol!", 4);
+    }
+    fb_write("\n", 1);
+  }
 
   return 0x42;
 }
