92f36680806fb6f0a983f487031e0b6f3065fb0b
Josh Simmons <josh.simmons@gmail.com>
2017-01-07 22:21:52 -0800

    Adding int_to_hex

--- string.c
+++ string.c
@@ -33,3 +33,28 @@ void int_to_dec(int i, char dec_str[12]) {
     dec_str[pos] = dec_str[pos + shift];
   }
 }
+
+// Leaving this at 12 so it's interoperable with int_to_dec.
+void int_to_hex(unsigned int i, char hex_str[12]) {
+  hex_str[0] = '0';
+  hex_str[1] = 'x';
+  hex_str[10] = 0;
+  hex_str[11] = 0;
+  for (int c = 9; c > 1; --c) {
+    char hex_char;
+    switch (i & 0xF) {
+      case 0xA:
+      case 0xB:
+      case 0xC:
+      case 0xD:
+      case 0xE:
+      case 0xF:
+        hex_char = 'A' + ((i & 0xF) - 0xA);
+        break;
+      default:
+        hex_char = '0' + (i & 0xF);
+    }
+    hex_str[c] = hex_char;
+    i >>= 4;
+  }
+}
--- string.h
+++ string.h
@@ -3,4 +3,6 @@
 
 void int_to_dec(int i, char dec_str[12]);
 
+void int_to_hex(unsigned int i, char hex_str[12]);
+
 #endif  // STRING_H
