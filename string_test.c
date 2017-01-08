5d7c3e4f9b9f0e1ade1bb7d9ad590e205ea13cde
Josh Simmons <josh.simmons@gmail.com>
2017-01-07 23:09:00 -0800

    Adding int_to_hex test.

--- string_test.c
+++ string_test.c
@@ -11,5 +11,8 @@ int main() {
   EXPECT_TRUE(strcmp("2147483647", dec_str) == 0);
   int_to_dec(-2147483648, dec_str);
   EXPECT_TRUE(strcmp("-2147483648", dec_str) == 0);
+
+  int_to_hex(0xCAFEBABE, dec_str);
+  EXPECT_TRUE(strcmp("0xCAFEBABE", dec_str) == 0);
   return 0;
 }
